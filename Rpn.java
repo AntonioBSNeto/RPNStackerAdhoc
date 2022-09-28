bb69912a534faa2c7761287513bdf7869edfd65e
AntonioBSNeto <61993051+AntonioBSNeto@users.noreply.github.com>
2022-09-27 21:05:27 -0300

    Task 2 - Scanning

--- Rpn.java
+++ Rpn.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.Stack;
 
+
 public class Rpn {
+
 	public static void main(String[] args) {
 
 		File inputFile = new File("Calc1.stk");
@@ -14,27 +16,46 @@ public class Rpn {
 			reader = new Scanner(inputFile);
 			while (reader.hasNextLine()) {
 				String line = reader.nextLine();
-
 				int fstTerm, sndTerm;
+        TokenType op;
 				if (line.matches("[0-9]+")) {
-					rpnStack.push(line);
+          System.out.println(new Token(TokenType.NUM, line)); 
+          rpnStack.push(line);
 				}else {
-					sndTerm = Integer.parseInt(rpnStack.pop());
-					fstTerm = Integer.parseInt(rpnStack.pop());
+          if (!rpnStack.isEmpty()){
+            sndTerm = Integer.parseInt(rpnStack.pop());
+          }else {
+            rpnStack.push("Error: Unexpected charecter: "+ line);
+            break;
+          }
+          if (!rpnStack.isEmpty()){
+            fstTerm = Integer.parseInt(rpnStack.pop());
+          }else {
+            rpnStack.push("Error: Unexpected charecter: "+ line);
+            break;
+          }
 					if(line.equals("+")) {
-						rpnStack.push(fstTerm+sndTerm+"");
+            rpnStack.push(fstTerm+sndTerm+"");
+            op = TokenType.PLUS;
 					} else if (line.equals("-")) {
+            op = TokenType.MINUS;
 						rpnStack.push(fstTerm-sndTerm+"");
 					} else if (line.equals("*")) {
+            op = TokenType.STAR;
 						rpnStack.push(fstTerm*sndTerm+"");
-					} else {
+					} else if (line.equals("/")) {
+            op = TokenType.SLASH;
 						rpnStack.push(((int)fstTerm/sndTerm)+"");
-					}
+					} else {
+            rpnStack.push("Error: Unexpected charecter: "+ line);
+            break;
+          }
+          System.out.println(new Token(op, line)); 
 				}		
 			}
 
 		} catch (IOException e) {
-			// 
+			/* DO NOTHING */
 		} finally {
 			if (reader != null) {
 				reader.close();
